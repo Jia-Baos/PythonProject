d3f4845a408a4aeb1a10facd4c4f0d84678d5e1a
Jia-Baos <18383827268@163.com>
2024-08-28 22:12:19 +0800

    2024-08-28 22:12

--- nanodet/pth/onnx2vino.py
+++
@@ -1,8 +0,0 @@
-from openvino.runtime import Core
-from openvino.runtime import serialize
- 
-ie = Core()
-onnx_model_path = r"./pth/nanodet.onnx"
-model_onnx = ie.read_model(model=onnx_model_path)
-# compiled_model_onnx = ie.compile_model(model=model_onnx, device_name="CPU")
-serialize(model=model_onnx, xml_path="./pth/nanodet.xml", bin_path="./pth/nanodet.bin", version="UNSPECIFIED")
